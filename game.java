039eb9a19776d35d00b4adeff0b673b42ee205c5
HannaVil <122802302+HannaVil@users.noreply.github.com>
2023-01-17 14:45:47 +0000

    Set up checkWin method

---
+++ game.java
@@ -0,0 +1,18 @@
+//This class is the main file. It contains the information on how the game will actually run
+public class game
+{
+  public static void main(String[] args)
+  {
+    checkWin();
+  }
+
+  /**
+  * This method yada yada
+  * @param
+  * @return 
+  */
+  public static void checkWin()
+  {
+    System.out.println("test");
+  }
+}
