a08ea62d30723ce2bca2b85ae754101d1427674c
SirB <62462739+braydenleung-Git@users.noreply.github.com>
2023-01-18 15:01:59 +0000

    restored

--- Grid.java
+++ Grid.java
@@ -1,18 +1,29 @@
+//This class creates a grid object and replaces them with player objects
+//Hanna and Brayden collaborated on this class (Hanna initialzed it and Brayden debugged it and changed it) 
 public class Grid
 {
+
+  //Initializing grid object
   public int[][] grid;
+  
+  //Constructor 
   public Grid()
   {
-    grid = new int[][]{
-        {0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0},
+    grid = new char[][]{
+        {'□','□','□','□','□','□','□'},
+        {'□','□','□','□','□','□','□'},
+        {'□','□','□','□','□','□','□'},
+        {'□','□','□','□','□','□','□'},
+        {'□','□','□','□','□','□','□'},
+        {'□','□','□','□','□','□','□'}
+        
+  
     };
   }
 
+  /** This method takes the grid object and prints it out. It does not have any parameters
+  * or returns.
+  */
   public void print()
   {
     for(int i = 0; i < grid.length; i++)
