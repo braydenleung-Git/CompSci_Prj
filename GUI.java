17253d2f4829a78ead4bb03f62ac985ac4ed4369
braydenleung-Git <brayden.leung@gmail.com>
2023-01-19 02:13:09 -0500

    started woring on Jframe

--- GUI.java
+++ GUI.java
@@ -1,5 +1,21 @@
-import java.awt.*; 
+import java.awt.*;
 import javax.swing.*;
 public class GUI {
-    
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Connect 4, By Brayden & Hanna");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize(screenSize);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel =  new JPanel();
+        panel.setLayout(new FlowLayout());
+        JLabel label = new JLabel("Test");
+        ImageIcon icon = new ImageIcon("/Iconpng");
+        frame.setIconImage(icon.getImage());
+        frame.add(panel);
+        panel.add(label);
+        frame.setVisible(true);
+
+    }
 }
