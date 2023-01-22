009df0ddc74ec7233ac076281049e254345f471a
braydenleung-Git <brayden.leung@gmail.com>
2023-01-21 19:17:05 -0500

    started working on console program

--- GUI.java
+++ GUI.java
@@ -1,28 +1,29 @@
 //set up all the prerequisites for the class
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.*;
 
 public class GUI {
-  private static final JPanel username_Layout = new JPanel();
   public static String Player1_Name = "Player 1";
   public static String Player2_Name = "Player 2";
-  private static JFrame frame = new JFrame("Connect 4, By Brayden & Hanna");
-
+  private static final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+  private static final JFrame frame = new JFrame("Connect 4, By Brayden & Hanna");
+  private static final JPanel main_Panel = new JPanel(new CardLayout());
+  private static final JPanel username_Layout = new JPanel();
+  private static final JPanel console_Layout= new JPanel();
   public static String userInput = null;
 
+
   public static void main(String[] args) {
+    //Set that when the user clicks cross button, it will kill the code
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    JPanel main_Panel = new JPanel(new CardLayout());
+
     setup_Username_Layout();
 
     //Adds the username_Layout to the main layout
-    main_Panel.add(username_Layout, BorderLayout.CENTER);
+    main_Panel.add(username_Layout,"Username");
     main_Panel.setLocation(frame.getHeight() / 2, frame.getWidth() / 2);
-
+    main_Panel.add(console_Layout, "Console");
     //add the main_panel(one that allow switching scene) to the JFrame
     frame.add(main_Panel, BorderLayout.CENTER);
     frame.pack();
@@ -55,7 +56,7 @@ public class GUI {
     JTextField player1_T = new JTextField("Insert your name");
     //This is used for turning the text input that the user put in the text box as the player name, by default it is set to "Player 1"
     player1_T.addActionListener(e -> {
-      if (!player1_T.getText().equals(null)) {
+      if (player1_T.getText() != null) {
         Player1_Name = player1_T.getText();
       }
     });
@@ -66,7 +67,7 @@ public class GUI {
     JTextField player2_T = new JTextField("Insert your name");
     //This is used for turning the text input that the user put in the text box as the player name, by default it is set to "Player 2"
     player2_T.addActionListener(e -> {
-      if (!player2_T.getText().equals(null)) {
+      if (player2_T.getText() != null) {
         Player2_Name = player2_T.getText();
       }
     });
@@ -89,11 +90,11 @@ public class GUI {
     //This adds a button to start the game
     JButton start_Button =  new JButton("Click to Start Game!");
     start_Button.setFont(new Font("Arial", Font.PLAIN, 15));
-    start_Button.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        //change layout
-      }
+    start_Button.addActionListener(e -> {
+      CardLayout cl = (CardLayout)(main_Panel.getLayout());
+      frame.setSize(screenSize);
+      frame.setLocation(0,0);
+      cl.show(main_Panel, "console");
     });
 
 
@@ -101,45 +102,46 @@ public class GUI {
     username_Layout.setLayout(new BorderLayout());
     username_Layout.add(game_Icon_TitleScreen, BorderLayout.NORTH);
     username_Layout.add(username, BorderLayout.CENTER);
+    username_Layout.add(start_Button,BorderLayout.SOUTH);
   }
 
   /**
    * This method is used to set up the console "Card" so that it makes switching between "cards" possible
    */
   public static void setup_Console_Layout() {
-    //Creates a JPanel called "Console"
-    JPanel console = new JPanel();
-    console.setLayout(new BorderLayout());
+
+    console_Layout.setLayout(new BorderLayout());
 
     //Set up console out put to text area
-    JTextArea console_Ouput = new JTextArea();
-    console_Ouput.setEditable(false);
+    JTextArea console_Output = new JTextArea();
+    console_Output.setEditable(false);
     PrintStream output = new PrintStream(new OutputStream() {
       @Override
       public void write(int b) {
-        console_Ouput.append(String.valueOf((char) b));
+        console_Output.append(String.valueOf((char) b));
       }
     });
     System.setOut(output);
     System.setErr(output);
-    console.add(console_Ouput,BorderLayout.PAGE_START);
+    console_Output.setBackground(Color.black);
+    console_Output.setFont(new Font(""));
+    console_Layout.add(console_Output,BorderLayout.PAGE_START);
 
-    //Set up console input to the textfield
+    //Set up console input to the text field
     JTextField console_Input = new JTextField();
-    console_Input.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        if(console_Input.equals(null))
-        {
-          userInput = " ";
-        }
-        else
-        {
-          userInput = console_Input.getText();
-        }
+    console_Input.addActionListener(e -> {
+      if(console_Input.equals(null))
+      {
+        userInput = " ";
+      }
+      else
+      {
+        userInput = console_Input.getText();
+        console_Input.setText("");
       }
     });
-
+    console_Input.setBackground(Color.decode("242424"));
+    console_Layout.add(console_Input,BorderLayout.PAGE_END);
   }
 }
 //Offline Copy of GPT code (Console)
@@ -174,7 +176,7 @@ public class Console {
 ==================================================================
     JTextField input = new JTextField();
     frame.add(input, BorderLayout.PAGE_END);
-    //(mod) the line below is redirecting the use for text inserted, does not represents listenser that listen to the input
+    //(mod) the line below is redirecting the use for text inserted, does not represent listener that listen to the input
     String userInput = input.getText();
 
 */
@@ -216,6 +218,60 @@ public class Console {
             frame.setVisible(true);
         }
     }
+
+*/
+//Offline Copy of GPT code(Switch Scene)
+/*
+================================================
+  public class SwitchScenes extends JFrame {
+      private JPanel cards;
+      private JTextField usernameField;
+      private JTextArea console;
+
+      public SwitchScenes() {
+          super("Switch Scenes");
+          setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          setLayout(new BorderLayout());
+
+          // create a JPanel to hold the cards
+          cards = new JPanel(new CardLayout());
+          add(cards, BorderLayout.CENTER);
+
+          // create the "username" card
+          JPanel usernameCard = new JPanel();
+          usernameCard.setLayout(new BoxLayout(usernameCard, BoxLayout.Y_AXIS));
+          JLabel usernameLabel = new JLabel("Enter your username:");
+          usernameField = new JTextField();
+          usernameCard.add(usernameLabel);
+          usernameCard.add(usernameField);
+          cards.add(usernameCard, "username");
+
+          // create the "console" card
+          JPanel consoleCard = new JPanel();
+          consoleCard.setLayout(new BorderLayout());
+          console = new JTextArea();
+          console.setEditable(false);
+          consoleCard.add(new JScrollPane(console), BorderLayout.CENTER);
+          cards.add(consoleCard, "console");
+
+          // create a button to switch to the "console" card
+          JButton switchButton = new JButton("Go to console");
+          switchButton.addActionListener(e -> {
+              String username = usernameField.getText();
+              console.append("Username: " + username + "\n");
+              CardLayout cl = (CardLayout)(cards.getLayout());
+              cl.show(cards, "console");
+          });
+          add(switchButton, BorderLayout.SOUTH);
+
+          pack();
+          setVisible(true);
+      }
+
+      public static void main(String[] args) {
+          new SwitchScenes();
+      }
+  }
 */
 
 
--- test.java
+++ test.java
@@ -1,8 +1,97 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
 
 public class test{
+
+    //This is testing the switch scene that ChatGPT provide used to understand how it works
+    public class SwitchScenes extends JFrame {
+        private JPanel cards;
+        private JTextField usernameField;
+        private JTextArea console;
+        public SwitchScenes(){
+            super("Switch Scenes");
+        }
+        public void main(String[] args) {
+
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setLayout(new BorderLayout());
+
+            // create a JPanel to hold the cards
+            cards = new JPanel(new CardLayout());
+            add(cards, BorderLayout.CENTER);
+
+            // create the "username" card
+            JPanel usernameCard = new JPanel();
+            usernameCard.setLayout(new BoxLayout(usernameCard, BoxLayout.Y_AXIS));
+            JLabel usernameLabel = new JLabel("Enter your username:");
+            usernameField = new JTextField();
+            usernameCard.add(usernameLabel);
+            usernameCard.add(usernameField);
+            cards.add(usernameCard, "username");
+
+            // create the "console" card
+            JPanel consoleCard = new JPanel();
+            consoleCard.setLayout(new BorderLayout());
+            console = new JTextArea();
+            console.setEditable(false);
+            consoleCard.add(new JScrollPane(console), BorderLayout.CENTER);
+            cards.add(consoleCard, "console");
+
+            // create a button to switch to the "console" card
+            JButton switchButton = new JButton("Go to console");
+            switchButton.addActionListener(e -> {
+                String username = usernameField.getText();
+                console.append("Username: " + username + "\n");
+                CardLayout cl = (CardLayout)(cards.getLayout());
+                cl.show(cards, "console");
+            });
+            add(switchButton, BorderLayout.SOUTH);
+
+            pack();
+            setVisible(true);
+        }
+    }
+    //This is testing the console that ChatGPT provide use to understand how it works
+    /*
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Console");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        JTextArea console = new JTextArea();
+        console.setEditable(false);
+        frame.add(console, BorderLayout.PAGE_START);
+
+        JTextField input = new JTextField();
+        frame.add(input, BorderLayout.PAGE_END);
+
+        // redirect standard output and error streams to the console
+        PrintStream out = new PrintStream(new OutputStream() {
+            @Override
+            public void write(int b) {
+                console.append(String.valueOf((char) b));
+            }
+        });
+        System.setOut(out);
+        System.setErr(out);
+
+        //Add action listener to the JTextField
+        input.addActionListener(e -> {
+            String userInput = input.getText();
+            console.append("\nUser input: " + userInput);
+            // code to process user input
+            input.setText("");
+        });
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+    */
+    //this is a demostration chatGPT about borderlayout in buttons?
+    /*
     public static void main(String[] args){
+
         JFrame frame = new JFrame("My Frame");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -26,6 +115,6 @@ public class test{
 
         frame.pack();
         frame.setVisible(true);
-    }
+    }*/
 
 }
